8757ceb11f19eed4997b7060c512b53663b205b4
kmwamasali <kmwamasali@gmail.com>
2021-05-26 13:10:22 +0300

    Exercise 2: Testing

--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -2,14 +2,38 @@ package stringutils
 
 import "testing"
 
-func TestUpper(t *testing.T) {
-	if Upper("abc") != "ABC" {
-		t.Error("expected 'abc' to equal 'ABC")
+func TestTableUpper(t *testing.T) {
+	var stringTests = []struct {
+		input string
+		expected string
+	}{
+			{"abc", "ABC"},
+			{"cde", "CDE"},
+			{"def", "DEF"},
+			{"ghi", "GHI"},
+	}
+
+	for _, test := range stringTests {
+		if output := Upper(test.input);output != test.expected {
+			t.Error("expected {} to equal {}", test.input, test.expected)
+		}
 	}
 }
 
-func TestLower(t *testing.T) {
-	if Lower("ABC") != "abc" {
-		t.Error("expected 'ABC' to equal 'abc'")
+func TestTableLower(t *testing.T) {
+	var stringTests = []struct {
+		input string
+		expected string
+	}{
+			{"ABC", "abc"},
+			{"CDE", "cde"},
+			{"DEF", "def"},
+			{"GHI", "ghi"},
+	}
+
+	for _, test := range stringTests {
+		if output := Lower(test.input);output != test.expected {
+			t.Error("expected {} to equal {}", test.input, test.expected)
+		}
 	}
 }
